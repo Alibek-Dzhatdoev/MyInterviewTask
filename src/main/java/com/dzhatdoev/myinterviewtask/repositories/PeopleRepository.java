fbd9f924aad05d384e3928dda8c3a538f04b7d99
SancezRick <alibekdzhatdoev@gmail.com>
2023-02-20 00:11:49 +0400

    Added annotations @Repository on repositories

--- src/main/java/com/dzhatdoev/myinterviewtask/repositories/PeopleRepository.java
+++ src/main/java/com/dzhatdoev/myinterviewtask/repositories/PeopleRepository.java
@@ -2,10 +2,12 @@ package com.dzhatdoev.myinterviewtask.repositories;
 
 import com.dzhatdoev.myinterviewtask.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
     Optional<Person> findByUsername(String name);
 
--- src/main/java/com/dzhatdoev/myinterviewtask/repositories/VoteRepository.java
+++ src/main/java/com/dzhatdoev/myinterviewtask/repositories/VoteRepository.java
@@ -4,9 +4,11 @@ import com.dzhatdoev.myinterviewtask.models.Person;
 import com.dzhatdoev.myinterviewtask.models.Quote;
 import com.dzhatdoev.myinterviewtask.models.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
     Vote findByQuoteAndVoter (Quote quote, Person voter);
 
